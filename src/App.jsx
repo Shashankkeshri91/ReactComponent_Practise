1ab129bde2660c1e2d0855e14286fb51422018c9
Shashank keshri <99127554+Shashankkeshri91@users.noreply.github.com>
2023-07-11 12:40:42 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Heading from "./components/Heading";
+import List from "./components/List";
+
+function App() {
+  return (
+    <>
+      <h1> Welcome To React Learning </h1>
+      <Heading />
+      <List />
+      {/* <List/> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/List.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function List() {
+  return (
+    <div>
+      <ul>
+        <li>Samosa</li>
+        <li>Pizza</li>
+        <li>Litti</li>
+        <li>Kachori</li>
+        <li>Pitha</li>
+        <li>Paneer</li>
+      </ul>
+    </div>
+  );
+}
+
+export default List;
